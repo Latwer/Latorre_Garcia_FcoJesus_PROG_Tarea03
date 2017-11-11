058dcd9b70b37d62d90855c64fa7c5d2cad62387
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2017-11-11 12:40:16 +0100

    Enumerado Dirección.

---
+++ src/tarea03/Direccion.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ *
+ * @author Latorre_Garcia_FranciscoJesus
+ */
+public enum Direccion {
+    //Direcciones posibles.
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE  
+}
