f55e8bcb360c37a8772eff93ab9f8b1703f703bb
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2017-11-11 19:42:51 +0100

    Creación clase Rey

---
+++ src/tarea03/Rey.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ *
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ *
+ * Creamos la clase Rey, cuyos atributos serán un color (del tipo enumerado
+ * Color) y posicion (de la clase Posicion), con la visibilidad adecuada.
+ */
+public class Rey {
+
+    private Color color;
+    private Posicion posicion;
+    
+}
