96bc2888ea4b13e0f98ba4f55d8e40e6efafe35c
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2017-11-11 18:06:56 +0100

    Creación de los métodos get y set para los atributos

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -10,10 +10,10 @@ package tarea03;
  * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
  */
 public class Posicion {
-
+    //Atributos de la clase posición privados.
     private int fila;
     private char columna;
-
+    //Constructor de la clase posición con sus valores.
     public Posicion(int fila, char columna) {
 
         if ((fila >= 1 && fila <= 8) && (columna >= 'a' && columna <= 'h')) {
@@ -22,7 +22,33 @@ public class Posicion {
         } else {
             this.fila = 1;
             this.columna = 'a';
-            System.out.printf("Error. No se ha insertado una posición válida.");
+            System.out.printf("Error. No se ha insertado una posición válida.\n");
+        }
+    }
+    /*Creamos los métodos get y set para los atributos. Recuerdemos que para el método set se debe 
+    tener en cuenta que los valores son correctos y si no se comportará lo mismo que el constructor.*/
+    public int getFila() {
+        return fila;
+    }
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setFila(int fila) {
+
+        if (fila >= 1 && fila <= 8) {
+            this.fila = fila;
+        } else {
+            this.fila = 1;
+            System.out.printf("Error. No se ha insertado una posición válida para la fila.\n");
+        }
+    }
+    public void setColumna(char columna) {
+        if (columna >= 'a' && columna <= 'h') {
+            this.columna = columna;
+        } else {
+            this.columna = 'a';
+            System.out.printf("Error. No se ha insertado una posición válida para la columna.\n");
         }
     }
 }
