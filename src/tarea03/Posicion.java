c83f2eb178331c7a826bc561cfc071c21151a5af
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2017-11-11 13:45:32 +0100

    Constructor clase Posición y valores a los atributos

--- src/tarea03/Posicion.java
+++ src/tarea03/Posicion.java
@@ -10,7 +10,19 @@ package tarea03;
  * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
  */
 public class Posicion {
+
     private int fila;
     private char columna;
-    
+
+    public Posicion(int fila, char columna) {
+
+        if ((fila >= 1 && fila <= 8) && (columna >= 'a' && columna <= 'h')) {
+            this.fila = fila;
+            this.columna = columna;
+        } else {
+            this.fila = 1;
+            this.columna = 'a';
+            System.out.printf("Error. No se ha insertado una posición válida.");
+        }
+    }
 }
