9ab25b0bd6ea4e16689fd812b8f246a5cfd8e8dd
Francisco Jesús Latorre García <piti_fran@hotmail.com>
2017-11-15 18:18:49 +0100

    Crear una clase llamada Principal que incluya un método main.

---
+++ src/tarea03/Principal.java
@@ -0,0 +1,12 @@
+package tarea03;
+
+/**
+ *
+ * @author Francisco Jesus Latorre Garcia <franlatorregarcia@gmail.com>
+ */
+public class Principal {
+    //Crear una clase llamada Principal que incluya un método main.
+    public static void main(String[] args) {
+        
+    }
+}
